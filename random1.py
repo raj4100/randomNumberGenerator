8a114a4b2f4fd0979488be7dc3995c6560e570f6
raj4100 <31617288+raj4100@users.noreply.github.com>
2017-09-16 17:40:57 +0530

    Update random1.py

--- random1.py
+++ random1.py
@@ -8,31 +8,30 @@ def random_number(start=0,last=1,count=1):
     s = seed()   
     m = 1
     c = 11
-    ra = []
+    values = []
     c1 = int(0.27*count)
-    c2 = count-c1
     var = 0
-    x = 0
-    r = 0
+    lower = 0
+    upper = 0
     mid = math.ceil((last-start+1)/2)
     for i in range(1,31):
         m = m*2
     m=m-1
     a=16807
-    while ((x < c1) or (r < c2)):
+    while ((lower < c1) or (upper < count-c1)):
         s=(a*s+c)%m
-        l=s%last
-        if l < start:
-            l=l+start
+        temp=s%last
+        if temp < start:
+            temp=temp+start
         
-        if ((l <= mid) and (x < c1)):
-            x=x+1
+        if ((temp <= mid) and (lower < c1)):
+            lower=lower+1
             var=var+1
-            ra.insert(var,l)
-        elif ((l > mid) and (r < c2)):
-            r=r+1
+            values.insert(var,temp)
+        elif ((temp > mid) and (upper < count-c1)):
+            upper=upper+1
             var=var+1
-            ra.insert(var,l)
+            values.insert(var,temp)
         else:
             continue
     return ra
